0165ab8649906640c02e4a4ed691770270686bc2
Nic30 <nic30@seznam.cz>
2020-08-10 11:58:13 +0200

    import distutils after setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-from distutils import log
 import os
 import platform
 from setuptools import setup, find_packages, Command
@@ -6,6 +5,7 @@ from setuptools.command.build_py import build_py
 from setuptools.command.egg_info import egg_info
 from setuptools.command.sdist import sdist
 from subprocess import check_call
+from distutils import log
 import sys
 
 
