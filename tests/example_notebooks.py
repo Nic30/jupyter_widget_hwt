666c5824e0f5061efecc1ae4ebb4d60e63fb764d
Nic30 <nic30@seznam.cz>
2020-10-03 11:51:45 +0200

    inline nbclient.tests.test_client.assert_notebooks_equal
    * to use assert from current TestCase

--- tests/example_notebooks.py
+++ tests/example_notebooks.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 
-from nbclient.tests.test_client import run_notebook, assert_notebooks_equal
+from nbclient.tests.test_client import run_notebook, normalize_output
 from nbconvert.exporters.exporter import ResourcesDict
 
 
@@ -26,7 +26,25 @@ class ExampleNotebooksTC(unittest.TestCase):
     def check_notebook(self, name, opts={}):
         input_file = os.path.join(ROOT, 'examples', name)
         input_nb, output_nb = run_notebook(input_file, opts, notebook_resources())
-        assert_notebooks_equal(input_nb, output_nb)
+        self.assert_notebooks_equal(input_nb, output_nb)
+
+    def assert_notebooks_equal(self, expected, actual):
+        # based on nbclient.tests.test_client.assert_notebooks_equal
+        expected_cells = expected['cells']
+        actual_cells = actual['cells']
+        self.assertEqual(len(expected_cells), len(actual_cells))
+    
+        for expected_cell, actual_cell in zip(expected_cells, actual_cells):
+            # Uncomment these to help debug test failures better
+            expected_outputs = expected_cell.get('outputs', [])
+            actual_outputs = actual_cell.get('outputs', [])
+            normalized_expected_outputs = list(map(normalize_output, expected_outputs))
+            normalized_actual_outputs = list(map(normalize_output, actual_outputs))
+            self.assertListEqual(normalized_expected_outputs, normalized_actual_outputs)
+    
+            expected_execution_count = expected_cell.get('execution_count', None)
+            actual_execution_count = actual_cell.get('execution_count', None)
+            self.assertEqual(expected_execution_count, actual_execution_count)
 
     def test_example_scheme_cuckoo_hash(self):
         self.check_notebook('example_scheme_cuckoo_hash.ipynb')
